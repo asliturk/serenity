0a80eb3b73abd83ac14b47c60983157efd8b7f1e
Hüseyin ASLITÜRK <asliturk@hotmail.com>
2020-06-13 14:06:21 +0300

    [DropThisCommitBeforeMerge] Workspace

--- Meta/refresh-serenity-qtcreator.sh
+++ Meta/refresh-serenity-qtcreator.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-if [ -z "$SERENITY_ROOT" ]
-then echo "Serenity root not set. Please set environment variable first. E.g. export SERENITY_ROOT=$(git rev-parse --show-toplevel)"
-fi
+export SERENITY_ROOT=`git rev-parse --show-toplevel`
 
 cd "$SERENITY_ROOT" || exit 1
-find . -name '*.ipc' -or -name '*.cpp' -or -name '*.idl' -or -name '*.c' -or -name '*.h' -or -name '*.S' -or -name '*.css' | grep -Fv Patches/ | grep -Fv Root/ | grep -Fv Ports/ | grep -Fv Toolchain/ | grep -Fv Base/ > serenity.files
+find . -name '*.ipc' -or -name '*.cpp' -or -name '*.idl' -or -name '*.c' -or -name '*.h' -or -name '*.S' -or -name '*.css' -or -name 'CMakeLists.txt' | grep -Fv Patches/ | grep -Fv Root/ | grep -Fv Ports/ | grep -Fv Toolchain/ | grep -Fv Base/ > serenity.files
